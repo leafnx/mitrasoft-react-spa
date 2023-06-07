cff4149ab7c24c01aae099f4b8e364b948250c5a
Leaf <lithfnx@gmail.com>
2023-06-07 16:27:40 +0300

    added simple error page element

--- src/router.js
+++ src/router.js
@@ -1,15 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import PageWrapper from './components/PageWrapper';
 import PostsPage from './routes/PostsPage';
 import UserPage from './routes/UserPage';
 import AboutPage from './routes/AboutPage';
-import PageWrapper from './components/PageWrapper';
+import ErrorPage from './routes/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <PageWrapper title='Error' element={<ErrorPage />} />,
     children: [
       {
         path: '/',
---
+++ src/routes/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import GoBack from "../components/GoBack";
+
+export default function ErrorPage() {
+  return(
+    <div className='text-center mt-5'>
+      <p>Nothing here</p>
+      <GoBack />
+    </div>
+  )
+}
